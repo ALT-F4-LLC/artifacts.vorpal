21cd8c70b13563584cc665c8ab3c1da87f5178e2
Erik Reinert <4638629+erikreinert@users.noreply.github.com>
2025-08-27 16:26:04 -0700

    feat(artifact): added nginx

--- src/vorpal.rs
+++ src/vorpal.rs
@@ -1,14 +1,20 @@
 use anyhow::Result;
-use vorpal_artifacts::{devenv::DevEnvBuilder, DEFAULT_SYSTEMS};
+use vorpal_artifacts::{artifact::nginx, DevEnvBuilder, DEFAULT_SYSTEMS};
 use vorpal_sdk::context::get_context;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let context = &mut get_context().await?;
 
+    // Development Environment
+
     DevEnvBuilder::new("dev", DEFAULT_SYSTEMS.to_vec())
         .build(context)
         .await?;
+
+    // Artifacts
+
+    nginx::build(context).await?;
 
     context.run().await
 }
